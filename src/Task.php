8cefc4db2f7ae195b9b3673792154a18ce7b85a4
krlv <eug.krlv@gmail.com>
2017-02-08 01:52:21 +0200

    Use Pool of Workers to create databases

---
+++ src/Task.php
@@ -0,0 +1,28 @@
+<?php
+namespace Threaded;
+
+class Task extends \Threaded
+{
+    private $user;
+
+    /**
+     * @param int $user User ID
+     */
+    public function __construct(int $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        try {
+            $divan = new Divan('172.17.0.1', 15984, 'root', 'X5u/07rm');
+            $divan->createDatabase('timeline_user_' . $this->user);
+        } catch (\Throwable $exception) {
+            echo $exception->getMessage(), PHP_EOL;
+        }
+    }
+}
