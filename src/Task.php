111ea5bc6741217ea66a4e7cfccabcd1b9536738
Eugene Kirillov <eugene.kirillov@pipedrive.com>
2017-02-09 17:14:24 +0200

    Use CouchDB cluster instead of single dev node

--- src/Task.php
+++ src/Task.php
@@ -19,7 +19,7 @@ class Task extends \Threaded
     public function run()
     {
         try {
-            $divan = new Divan('172.17.0.1', 15984, 'root', 'X5u/07rm');
+            $divan = new Divan('172.17.0.1', 5984, 'root', 'X5ud07rm');
             $divan->createDatabase('timeline_user_' . $this->user);
         } catch (\Throwable $exception) {
             echo $exception->getMessage(), PHP_EOL;
