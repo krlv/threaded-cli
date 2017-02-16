19c370b59c9bbeca2b3c54c9ea4b540bcf35cacb
krlv <eug.krlv@gmail.com>
2017-02-16 02:50:19 +0200

    Modify task runner to process channels by team

--- src/Task.php
+++ src/Task.php
@@ -9,7 +9,7 @@ class Task extends \Threaded
     private $team;
 
     /**
-     * @param int $team Team ID
+     * @param int $team
      */
     public function __construct(int $team)
     {
@@ -23,9 +23,44 @@ class Task extends \Threaded
     {
         try {
             $couch = new Client('172.17.0.1', 5984, 'root', 'X5ud07rm');
-            echo 'Task will work with teams from shard ', $this->team, PHP_EOL;
+
+            $limit = 100;
+            $skip  = 0;
+            $total = 0;
+
+            do {
+                $docs = $this->getChannels($couch, $limit, $skip);
+
+                $total += count($docs);
+                $skip  += $limit;
+            } while (count($docs) == $limit);
+
+            echo 'Worker ', $this->team, ' has processed ', $total, ' channels', PHP_EOL;
         } catch (ExceptionInterface $exception) {
             echo $exception->getMessage(), PHP_EOL;
         }
+    }
+
+    private function getChannels(Client $couch, int $limit, int $skip): array
+    {
+        $query = [
+            'selector' => [
+                '$and' => [
+                    [
+                        'team_id'  => [
+                            '$gte' => $this->team * 125,
+                        ],
+                    ],
+                    [
+                        'team_id' => [
+                            '$lt' => ($this->team + 1) * 125,
+                        ],
+                    ],
+                ],
+            ],
+            'limit' => $limit,
+            'skip'  => $skip,
+        ];
+        return $couch->findDocuments('channels', $query)['docs'];
     }
 }
